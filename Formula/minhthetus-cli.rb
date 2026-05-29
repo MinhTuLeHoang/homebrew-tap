5f3e4ab894f113b66a0d0d5c525d14db788f7e21
tulhm <tulhm@vng.com.vn>
2026-05-29 13:32:38 +0700

    feat: add minhthetus-cli formula at v1.0.0-rc7

---
+++ Formula/minhthetus-cli.rb
@@ -0,0 +1,23 @@
+class MinhthetusCli < Formula
+  desc "A professional, high-performance CLI for developer productivity"
+  homepage "https://github.com/MinhTuLeHoang/minhthetus-cli"
+  url "https://github.com/MinhTuLeHoang/minhthetus-cli/archive/refs/tags/v1.0.0-rc7.tar.gz"
+  sha256 "d970179cc15f05d4d95e22587a85c06dce808f6d1ad2d22f4720c4b2727da18d"
+  license "ISC"
+
+  depends_on "go" => :build
+
+  def install
+    # Inject the version and current build date during compilation
+    build_date = Time.now.strftime("%Y-%m-%d")
+    ldflags = "-s -w -X github.com/MinhTuLeHoang/minhthetus-cli/cmd.Version=v#{version} -X github.com/MinhTuLeHoang/minhthetus-cli/cmd.BuildDate=#{build_date}"
+
+    system "go", "build", "-ldflags", ldflags, "-o", "minhthetus-cli", "main.go"
+    
+    bin.install "minhthetus-cli"
+  end
+
+  test do
+    assert_match "version v#{version}", shell_output("#{bin}/minhthetus-cli --version")
+  end
+end
